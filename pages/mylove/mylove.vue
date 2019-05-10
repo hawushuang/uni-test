ea421a90d13400ce8301ad74908047ec473ec659
Administrator <guadan0711@126.com>
2019-05-10 13:18:24 +0800

    修改

--- pages/mylove/mylove.vue
+++ pages/mylove/mylove.vue
@@ -3,6 +3,17 @@
 		<image class="logo" src="/static/slogen.gif"></image>
 		<view class="example-title">距离妹妹解脱还剩</view>
 		<uni-countdown color="#FFFFFF" background-color="#00B26A" border-color="#00B26A" :day="day" :hour="hour" :minute="0" :show-colon="false"></uni-countdown>
+		<view class="minie">{{ word }}</view>
+		<view class="uni-list">
+			<view class="uni-list-cell" v-for="(value, key) in list" :key="key">
+				<view class="uni-media-list">
+					<view class="uni-media-list-body">
+						<view class="uni-media-list-text-top">{{ value.title }}</view>
+						<view class="uni-media-list-text-bottom uni-ellipsis">{{ value.content }}</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -12,7 +23,20 @@ export default {
 	data() {
 		return {
 			day: 1,
-			hour: 1
+			hour: 1,
+			word: 'aaaaaa',
+			list: [
+				{
+					title: '木屋',
+					content: '想要这样一间小木屋，夏天挫冰吃瓜，冬天围炉取暖。',
+					img: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90'
+				},
+				{
+					title: 'CBD',
+					content: '烤炉模式的城，到黄昏，如同打翻的调色盘一般。',
+					img: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90'
+				}
+			]
 		};
 	},
 	onLoad() {
@@ -24,6 +48,8 @@ export default {
 		this.day = Math.floor(leftSecond / (60 * 60 * 24));
 		leftSecond = leftSecond % (3600 * 24);
 		this.hour = Math.floor(leftSecond / 3600);
+		
+		this.word = "丢雷斗魔"
 	},
 	methods: {},
 	components: { uniCountdown }
@@ -46,5 +72,9 @@ export default {
 	text-align: center;
 	font-size: 36upx;
 	color: #8f8f94;
+}
+.minie {
+	font-size: 36upx;
+	color: #000;
 }
 </style>
